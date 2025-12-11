3f075a81f0330e3cfb033660212b5c2e339020a5
keeplooking6 <1422910966@qq.com>
2025-12-11 15:20:39 +0800

    feat: view the function for measuring program execution time

--- 01_TheApplicationOfAdvancedGrammer/_01-05_comprehensive_question/question_02.py
+++ 01_TheApplicationOfAdvancedGrammer/_01-05_comprehensive_question/question_02.py
@@ -3,7 +3,21 @@
 使用生成器来高效地（指内存使用方面）查找所有包含 "ERROR" 关键词的日志行
 并与一次性将整个文件读入内存的列表方法进行对比
 """
+import time
 
+
+# from .._01_decorator._02_standard_writing_style import decorator
+def decorator(func):
+    def wrapper(*args,**kwargs):
+        start_time = time.time()
+        result = func(*args,**kwargs)
+        print('2222')
+        end_time = time.time()
+        print(f"{func.__name__}所需时间:",end_time-start_time)
+        return result
+    print('1111')
+
+    return wrapper
 # 读取huge_log.txt文件（高效做法）
 def read_file():
     with open("huge_log.txt",'r') as f:
@@ -12,13 +26,21 @@ def read_file():
                 yield f_content.strip()
 
 # 使用生成器查找包含 "ERROR" 关键词的日志行
-gen = read_file()
-for g in gen:
-    print(g)
+@decorator
+def use_generator():
+    gen = read_file()
+    for g in gen:
+        print(g)
 
 # 列表生成式（低效做法）
-with open("huge_log.txt",'r') as f:
-    all_lines = f.readlines()
-result = [error_lines.strip('\n') for error_lines in all_lines if 'ERROR' in error_lines]
-print(result)
+@decorator
+def use_list_comprehensions():
+    with open("huge_log.txt",'r') as f:
+        all_lines = f.readlines()
+    result = [error_lines.strip('\n') for error_lines in all_lines if 'ERROR' in error_lines]
+    print(result)
 
+#  数据量太少，看不出差别
+use_generator()
+print('==========')
+use_list_comprehensions()
